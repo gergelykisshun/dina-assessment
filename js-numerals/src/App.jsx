065a8eaba063a927c6f2ac339d22e6f677b4f5d4
GergelyKiss <gergely.kiss.hun@gmail.com>
2022-05-23 20:54:03 +0200

    app is ready

--- js-numerals/src/App.jsx
+++ js-numerals/src/App.jsx
@@ -1,116 +1,23 @@
 import React, { useState } from 'react';
 import './App.css';
+import phrasify from './logic/phrasify';
 
 const App = () => {
 
-  const pharisifyHelper = {
-    "1": "one",
-    "2": "two",
-    "3": "three",
-    "4": "four",
-    "5": "five",
-    "6": "six",
-    "7": "seven",
-    "8": "eight",
-    "9": "nine",
-    "10": "ten",
-    "11": "eleven",
-    "12": "twelve",
-    "13": "thirteen",
-    "14": "fourteen",
-    "15": "fifteen",
-    "16": "sixteen",
-    "17": "seventeen",
-    "18": "eighteen",
-    "19": "nineteen",
-    "20": "twenty",
-    "30": "thirty",
-    "40": "forty",
-    "50": "fifty",
-    "60": "sixty",
-    "70": "seventy",
-    "80": "eighty",
-    "90": "ninety",
-    "100": "hundred",
-    "1000": "thousand",
-    "1000000": "million"
-  }
-
-
   const [num, setNum] = useState('');
 
-
-  const phrasify = (num) => {
-    let strArr = num.toString().split('');
-    let resultStr = '';
-    while (strArr.length > 0){
-      switch(strArr.length){
-        case 1:
-          resultStr += pharisifyHelper[strArr[0]]
-          break;
-        case 2:
-          if(strArr[0] === '1'){
-            resultStr += pharisifyHelper[strArr.join('')]
-            strArr.shift();
-          }else if (strArr[1] === '0'){
-            resultStr += pharisifyHelper[strArr.join('')]
-            strArr.shift();
-          } else {
-            resultStr += `${pharisifyHelper[parseInt(strArr[0]) * 10]}-`
-          };
-          break;
-        case 3:
-          resultStr += `${pharisifyHelper[parseInt(strArr[0])]} hundred and `;
-          break;
-        case 4:
-          resultStr += `${pharisifyHelper[parseInt(strArr[0])]} thousand `;
-          break;
-        case 5:
-          if(strArr[0] === '1'){
-            resultStr += `${pharisifyHelper[`${strArr[0]}${strArr[1]}`]} thousand `;
-            strArr.shift();
-          }else if (strArr[1] === '0'){
-            resultStr += `${pharisifyHelper[`${strArr[0]}${strArr[1]}`]} thousand `
-            strArr.shift();
-          } else {
-            resultStr += `${pharisifyHelper[parseInt(strArr[0]) * 10]} thousand `
-          };
-          break;
-        case 6:
-          if(strArr[1] === '0' && strArr[2] === '0'){
-            resultStr += `${pharisifyHelper[parseInt(strArr[0])]} hundred thousand `;
-            strArr.shift();
-            strArr.shift();
-          } else {
-            resultStr += `${pharisifyHelper[parseInt(strArr[0])]} hundred `;
-          }
-          break;
-        case 7:
-          resultStr += `${pharisifyHelper[strArr[0]]} million `
-          break;
-        case 8:
-          if(strArr[0] === '1'){
-            resultStr += `${pharisifyHelper[`${strArr[0]}${strArr[1]}`]} million `;
-            strArr.shift();
-          }else if (strArr[1] === '0'){
-            resultStr += `${pharisifyHelper[`${strArr[0]}${strArr[1]}`]} million `;
-            strArr.shift();
-          } else {
-            resultStr += `${pharisifyHelper[`${strArr[0]}${strArr[1]}`]} million `;
-          };
-          break;
-        default:
-      }
-      // necessary last step so while doesn't go infinite
-      strArr.shift();
+  const inputHandler = (e) => {
+    if(num.length < 8){
+      setNum(e.target.value)
+    } else {
+      setNum(e.target.value.slice(0, 8))
     }
-    // returns the phrasified version of the number
-    return resultStr;
-  };
+  }
+
   return (
     <section className='wrapper'>
       <div className='content'>
-        <input type="text" onChange={(e) => setNum(e.target.value)} value={num}/>
+        <input className='input-style' type="text" onChange={inputHandler} value={num}/>
         <h2>{phrasify(num)}</h2>
       </div> 
     </section>
