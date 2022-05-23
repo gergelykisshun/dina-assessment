d5f14410caa7050b038e1d57b6ce813bf497a3c1
GergelyKiss <gergely.kiss.hun@gmail.com>
2022-05-23 19:55:19 +0200

    helper object typed

--- js-numerals/src/App.jsx
+++ js-numerals/src/App.jsx
@@ -2,17 +2,52 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+
+  const pharisifyHelper = {
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
+    "10": "ten",
+    "11": "eleven",
+    "12": "twelve",
+    "13": "thirteen",
+    "14": "fourteen",
+    "15": "fifteen",
+    "16": "sixteen",
+    "17": "seventeen",
+    "18": "eighteen",
+    "19": "nineteen",
+    "20": "twenty",
+    "30": "thirty",
+    "40": "forty",
+    "50": "fifty",
+    "60": "sixty",
+    "70": "seventy",
+    "80": "eighty",
+    "90": "ninety",
+    "100": "hundred",
+    "1000": "thousand",
+    "1000000": "million"
+  }
+
+
   const [num, setNum] = useState('');
 
 
-  const phraseNumber = (num) => {
-    return num.toUpperCase();
+  const phrasify = (num) => {
+    
   };
   return (
     <section className='wrapper'>
       <div className='content'>
         <input type="text" onChange={(e) => setNum(e.target.value)} value={num}/>
-        <h2>{phraseNumber(num)}</h2>
+        <h2>{phrasify(num)}</h2>
       </div> 
     </section>
   )
